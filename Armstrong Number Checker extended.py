7391128a7109382a56a04770d81412c37a5f9929
Harikiran-PV2037 <harikiran.dilip@gmail.com>
2025-11-14 00:07:38 +0530

    Armstrong Number Checker Range

---
+++ Armstrong Number Checker extended.py
@@ -0,0 +1,16 @@
+#Armstrong numbers in a range given by user
+
+number_range= int(input("Enter your range:"))
+print("Armstrong Numbers in your number range are :")
+for i in range(number_range+1):
+    store_val=i
+    len_number = len(str(abs(i)))
+    sum=0
+    check_number=i
+    for j in range(len_number):
+        num2 = store_val % 10
+        sum = sum + num2 ** len_number
+        store_val = store_val // 10
+    if sum == check_number:
+        print(check_number) 
+    
